1474a9572ea79d9ebe58e3313ea8d8132f7cf8ea
Sarthak Gudwani <gudwanisarthak2000@gmail.com>
2024-05-05 02:39:35 +0530

    Initialized the expo app

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'outfit': require('./assets/fonts/Outfit-Regular.ttf'),
+    'outfit-bold': require('./assets/fonts/Outfit-Bold.ttf'),
+    'outfit-medium': require('./assets/fonts/Outfit-SemiBold.ttf'),
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily:'outfit'}}>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily:'outfit-bold'}}>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily:'outfit-medium'}}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
