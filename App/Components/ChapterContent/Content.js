f02817d2a654789438da824f9fff90ba96840040
Sarthak Gudwani <gudwanisarthak2000@gmail.com>
2024-05-08 01:11:38 +0530

    Added Chapter Heading with a slider

--- App/Components/ChapterContent/Content.js
+++ App/Components/ChapterContent/Content.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, Dimensions } from 'react-native'
 import React from 'react'
 import ProgressBar from './ProgressBar'
 
@@ -6,7 +6,12 @@ export default function Content({content}) {
   return (
     <View style={{padding:20}}>
         <ProgressBar contentLength={content?.length} contentIndex={1}/>
-        
+        <FlatList horizontal={true} pagingEnabled showsHorizontalScrollIndicator={false} data={content} renderItem={({item}) => (
+            // Screen Width 
+            <View style={{width:Dimensions.get('screen').width*0.89}}>
+                <Text style={{fontFamily:'outfit-medium', fontSize:22, marginTop:15}}>{item.heading}</Text>
+            </View>
+        )}/>
     </View>
   )
 }
