5d5c5e12de8aafaf485e501bbd5b84039bc1a442
Sarthak Gudwani <gudwanisarthak2000@gmail.com>
2024-05-08 00:28:26 +0530

    Added Navigation on the Chapter List Items

---
+++ App/Screens/ChapterContentScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ChapterContentScreen() {
+  return (
+    <View>
+      <Text>ChapterContentScreen</Text>
+    </View>
+  )
+}
