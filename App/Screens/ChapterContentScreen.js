d2524dca3becdac0f48d6609ddecd656171c754d
Sarthak Gudwani <gudwanisarthak2000@gmail.com>
2024-05-09 04:28:49 +0530

    Save User Points and User Details

--- App/Screens/ChapterContentScreen.js
+++ App/Screens/ChapterContentScreen.js
@@ -4,12 +4,16 @@ import Content from '../Components/ChapterContent/Content'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import { MarkChapterCompleted } from '../Services';
 import { CompleteChapterContext } from '../Context/CompleteChapterContext';
+import { UserPointsContext } from '../Context/UserPointsContext';
+import { useUser } from '@clerk/clerk-expo';
 
 export default function ChapterContentScreen() {
   const navigation = useNavigation();
   const param = useRoute().params;
   const {isChapterComplete, setIsChapterComplete} = useContext(CompleteChapterContext);
-
+  const {userPoints, setUserPoints} = useContext(UserPointsContext);
+  const {user} = useUser();
+  
   useEffect(() => {
     console.log("Chapter Id: ", param.chapterId);
     console.log("Record Id: ", param.userCourseRecordId);
@@ -17,7 +21,8 @@ export default function ChapterContentScreen() {
 
   const onChapterFinish = () => {
     // navigation.goBack()
-    MarkChapterCompleted(param.chapterId, param.userCourseRecordId)
+    const totalPoints =Number(userPoints) + param.content?.length * 10;
+    MarkChapterCompleted(param.chapterId, param.userCourseRecordId, user.primaryEmailAddress.emailAddress, totalPoints)
       .then(response => {
         // console.log(response)
         if (response)
--- App/Screens/HomeScreen.js
+++ App/Screens/HomeScreen.js
@@ -1,17 +1,46 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import Header from '../Components/HomeScreen/Header'
 import Colors from '../Utils/Colors'
 import CourseList from '../Components/HomeScreen/CourseList'
+import { useAuth, useUser } from '@clerk/clerk-expo'
+import { createNewUser } from '../Services'
+import { UserPointsContext } from '../Context/UserPointsContext'
 
 export default function HomeScreen() {
+    const { isLoaded, signOut } = useAuth();
+    const { user } = useUser();
+    const { userPoints, setUserPoints } = useContext(UserPointsContext);
+
+    useEffect(() => {
+        user && createUser();
+    }, [user])
+
+    const createUser = () => {
+        if (user) {
+            createNewUser(user.fullName, user.primaryEmailAddress.emailAddress, user.imageUrl)
+                .then(response => {
+                    if (response) {
+                        GetUser()
+                    }
+                })
+        }
+    }
+
+    const GetUser = () => {
+        getUserDetail(user.primaryEmailAddress.emailAddress)
+        .then(response => {
+            console.log("Get User Detail : ", response.userDetail?.point);
+            setUserPoints(response.userDetail?.point)
+        })
+    }
     return (
         <View>
             <View style={{ backgroundColor: Colors.PRIMARY, height: 250, padding: 20 }}>
                 <Header />
             </View>
             <View style={{ padding: 20 }}>
-                <View style={{marginTop:-90}}>
+                <View style={{ marginTop: -90 }}>
                     <CourseList level={"Basic"} />
                 </View>
                 <CourseList level={"Advance"} />
